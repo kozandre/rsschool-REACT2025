4d0ffdb8a8848b23d62489aad522d04b25a614df
Andrey <an.kozimirov@gmail.com>
2025-07-13 14:35:26 +0300

    feat: add error button

--- rs-react-app/src/components/ErrorButton/ErrorButton.tsx
+++ rs-react-app/src/components/ErrorButton/ErrorButton.tsx
@@ -1,8 +1,22 @@
 import { Component } from 'react';
+import classes from './ErrorButton.module.scss';
 
 class ErrorButton extends Component {
+  constructor(props: {}) {
+    super(props);
+    this.errorHandler = this.errorHandler.bind(this);
+  }
+
+  errorHandler = () => {
+    throw new Error();
+  };
+
   render() {
-    return <div>Error</div>;
+    return (
+      <button className={classes.errorButton} onClick={this.errorHandler}>
+        Error Button
+      </button>
+    );
   }
 }
 
